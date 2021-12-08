aaccbabe2d48653b587e9d5eec508d0daf9086bf
Alisson Martineli <martineli.alisson@gmail.com>
2021-12-08 18:21:21 -0300

    Added url entity

---
+++ src/entities/URL.ts
@@ -0,0 +1,11 @@
+class URL {
+  url: string
+  shortenedURL: string
+
+  constructor(url: string, shortenedURL: string) {
+    this.url = url
+    this.shortenedURL = shortenedURL
+  }
+}
+
+export default URL
