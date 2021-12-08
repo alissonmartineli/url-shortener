b4590657560626af28ab06bc32a1ec94b39b3c62
Alisson Martineli <martineli.alisson@gmail.com>
2021-12-08 18:24:52 -0300

    Added controller

---
+++ src/controllers/URLShortenerController.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from 'express'
+import URLShortenerService from '../services/URLShortenerService'
+import constants from '../configs/constants'
+
+class URLShortenerController {
+  private service: URLShortenerService
+
+  constructor(service: URLShortenerService) {
+    this.service = service
+  }
+
+  async shorten(req: Request<{}, {}, { url: string }>, res: Response) {
+    const url = req.body.url
+    const shortenedURL = await this.service.shorten(url)
+    res.json({ shortenedURL: `${constants.API_URL}/${shortenedURL}` })
+  }
+
+  async get(req: Request<{ shortenedURL: string }>, res: Response) {
+    const shortenedURL = req.params.shortenedURL
+    const url = await this.service.get(shortenedURL)
+    if (url) {
+      res.json({ url: url.url })
+    } else {
+      res.status(404).send()
+    }
+  }
+}
+
+export default URLShortenerController
